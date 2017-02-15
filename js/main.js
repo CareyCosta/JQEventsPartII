35ead779f3de592c2d03d92596771733c20323b9
Carey Costa <careyxcosta@gmail.com>
2017-02-15 17:34:19 -0500

    when button is clicked, modal window fades in

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  // When the user presses the .signin button, fade in the modal window
+  $('.signin').on('click', function(){
+    $('.modal').fadeIn('slow');
+  });
+});
